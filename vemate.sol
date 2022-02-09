502d62dc37e9a33072268da3fd6cf6b413fa3f34
kausar75 <abu.kausar@dynamiconsulting.co.uk>
2022-02-09 21:25:02 +0600

    updated

--- vemate.sol
+++ vemate.sol
@@ -1,21 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 contract vemate {
-    uint purchaseToken;
+    uint public purchaseToken = 10000000000;
     uint purchasedTime = block.timestamp;
-    uint unlockedToken;
-    //mapping(address => uint) public unlockedToken;
+    string public name = "Vemate";
+    string public symbol = "VMC";
+    mapping(address => uint) public unlockedToken;
+
+    event Transfer(address indexed from, address indexed to, uint amount);
+
+    constructor(){
+        unlockedToken[msg.sender] = purchaseToken * 10 / 100;  
+    }
+
+    function transfer(address to, uint amount) public returns(bool){
+        //have to call getBalance method
+        require(unlockedToken > amount, 'Insufficient amount');
+        unlockedToken[to] += amount;
+        unlockedToken[msg.sender] -= amount;
+
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
 
     function getBalance(address owner) public returns(uint) {
         uint endTime = block.timestamp;
         require(endTime > purchasedTime, 'There is no unlocked token to show');
         uint dayDifference = (endTime - purchasedTime) / (3600*24);
 
-        if(dayDifference > 0) {
-            unlockedToken = purchaseToken * 10 / 100;
-            return unlockedToken;
-        }
-        else if(dayDifference > 21){
+        if(dayDifference > 21){
             unlockedToken = purchaseToken * 20 / 100;
             return unlockedToken;
         }

3236947e6e6f0b32fbc0f3ca67b97b0a9c60a058
kausar75 <abukausar33@gmail.com>
2022-06-16 16:07:02 +0600

    updated
    - removed FixedPoint library as there is no use of that
    - removed constant keyword from _NAME, _SYMBOL, _DECIMALS and TOTAL_SUPPLY
    - and also intialized the value into the constructor

--- contracts/VemateToken.sol
+++ contracts/VemateToken.sol
@@ -338,77 +338,6 @@ library SafeMath {
     }
 }
 
-// a library for handling binary fixed point numbers (https://en.wikipedia.org/wiki/Q_(number_format))
-library FixedPoint {
-    // range: [0, 2**112 - 1]
-    // resolution: 1 / 2**112
-    struct uq112x112 {
-        uint224 _x;
-    }
-
-    // range: [0, 2**144 - 1]
-    // resolution: 1 / 2**112
-    struct uq144x112 {
-        uint256 _x;
-    }
-
-    uint8 private constant RESOLUTION = 112;
-    uint256 private constant Q112 = uint256(1) << RESOLUTION;
-    uint256 private constant Q224 = Q112 << RESOLUTION;
-
-    // encode a uint112 as a UQ112x112
-    function encode(uint112 x) internal pure returns (uq112x112 memory) {
-        return uq112x112(uint224(x) << RESOLUTION);
-    }
-
-    // encodes a uint144 as a UQ144x112
-    function encode144(uint144 x) internal pure returns (uq144x112 memory) {
-        return uq144x112(uint256(x) << RESOLUTION);
-    }
-
-    // divide a UQ112x112 by a uint112, returning a UQ112x112
-    function div(uq112x112 memory self, uint112 x) internal pure returns (uq112x112 memory) {
-        require(x != 0, 'FixedPoint: DIV_BY_ZERO');
-        return uq112x112(self._x / uint224(x));
-    }
-
-    // multiply a UQ112x112 by a uint, returning a UQ144x112
-    // reverts on overflow
-    function mul(uq112x112 memory self, uint256 y) internal pure returns (uq144x112 memory) {
-        uint256 z;
-        require(y == 0 || (z = uint256(self._x) * y) / y == uint256(self._x), 'FixedPoint: MULTIPLICATION_OVERFLOW');
-        return uq144x112(z);
-    }
-
-    // returns a UQ112x112 which represents the ratio of the numerator to the denominator
-    // equivalent to encode(numerator).div(denominator)
-    function fraction(uint112 numerator, uint112 denominator) internal pure returns (uq112x112 memory) {
-        require(denominator > 0, 'FixedPoint: DIV_BY_ZERO');
-        return uq112x112((uint224(numerator) << RESOLUTION) / denominator);
-    }
-
-    // decode a UQ112x112 into a uint112 by truncating after the radix point
-    function decode(uq112x112 memory self) internal pure returns (uint112) {
-        return uint112(self._x >> RESOLUTION);
-    }
-
-    // decode a UQ144x112 into a uint144 by truncating after the radix point
-    function decode144(uq144x112 memory self) internal pure returns (uint144) {
-        return uint144(self._x >> RESOLUTION);
-    }
-
-    // take the reciprocal of a UQ112x112
-    function reciprocal(uq112x112 memory self) internal pure returns (uq112x112 memory) {
-        require(self._x != 0, 'FixedPoint: ZERO_RECIPROCAL');
-        return uq112x112(uint224(Q224 / self._x));
-    }
-
-    // square root of a UQ112x112
-    function sqrt(uq112x112 memory self) internal pure returns (uq112x112 memory) {
-        return uq112x112(uint224(SafeMath.sqrt(uint256(self._x)) << 56));
-    }
-}
-
 interface IUniswapV2Factory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
@@ -610,7 +539,6 @@ interface IUniswapV2Pair {
 
 
 contract Vemate is IBEP20, Ownable{
-    using FixedPoint for *;
 
     struct FeeWallet {
         address payable treasury;
@@ -630,17 +558,17 @@ contract Vemate is IBEP20, Ownable{
 
     IUniswapV2Router02 public uniswapV2Router;
 
-    string private constant _NAME = "Vemate";
-    string private constant _SYMBOL = "VMT";
+    string private _NAME;
+    string private _SYMBOL;
 
-    uint8 private constant _DECIMALS = 18;
+    uint8 private _DECIMALS;
     uint8 public constant MAX_FEE_PERCENT = 5;
     bool private inSwapAndLiquify;
     bool public swapAndLiquifyEnabled = true;
 
     address public uniswapV2Pair;
 
-    uint256 private constant TOTAL_SUPPLY = 15 * 10**7 * 10**_DECIMALS; // 150 million;
+    uint256 private TOTAL_SUPPLY; 
 
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
@@ -663,6 +591,12 @@ contract Vemate is IBEP20, Ownable{
         require(router != address(0), "Router must be set");
         require(treasuryAddress != address(0), "Treasury wallet must be set");
         require(charityAddress != address(0), "Charity wallet must be set");
+
+        _NAME = "Vemate";
+        _SYMBOL = "VMT";
+        _DECIMALS = 18;
+
+        TOTAL_SUPPLY = 15 * 10**7 * 10**_DECIMALS; // 150 million;
 
         _isPrivileged[owner()] = true;
         _isPrivileged[address(this)] = true;
